a14a924dc27c5753965548333bcd19d6979badbd
optimusprime <s143522@student.hb.se>
2017-02-23 10:25:57 +0100

    lite kommentarer på request

--- CodebustersAppWMU2/Services/RequestHelper.cs
+++ CodebustersAppWMU2/Services/RequestHelper.cs
@@ -13,16 +13,18 @@ namespace CodebustersAppWMU2.Services
     class RequestHelper
     {
         /*
-         * Hopefully this code works
-         * 
+         * Hopefully this code works. When calling this method you need to specify which type is intended
+         * for the request, and return type. Don't forget to use await when calling this method as it returns
+         * a Task, also the parameter takes in the url request: ...api/{request}.
+         * Call example: var elements = await GetRequest<TaskDto>("tasks");
          */
-        public async Task<IEnumerable<T>> GetRequest<T>(string requestTo)
+        public async Task<IEnumerable<T>> GetRequest<T>(string request)
         {
             try
             {
                 HttpClient client = TaskManagerHttpClient.GetClient();
 
-                HttpResponseMessage response = await client.GetAsync("api/" + requestTo);
+                HttpResponseMessage response = await client.GetAsync("api/" + request);
 
                 if (response.IsSuccessStatusCode)
                 {
@@ -39,7 +41,6 @@ namespace CodebustersAppWMU2.Services
             {
                 return null;
             }
-
 
         }
     }
